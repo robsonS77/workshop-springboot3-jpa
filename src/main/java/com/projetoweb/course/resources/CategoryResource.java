f40de63596c36669c9091259542503f66b3914b0
Robson Sousa <rcls7723@gmail.com>
2023-02-07 19:59:57 -0300

    Category

---
+++ src/main/java/com/projetoweb/course/resources/CategoryResource.java
@@ -0,0 +1,34 @@
+package com.projetoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoweb.course.entities.Category;
+import com.projetoweb.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
